02be29cb6f243dda69db795339edde93ef9bec4b
ravinduyas <ravindu.yasanka21@gmail.com>
2024-08-25 09:03:13 +0530

    Springboot project created

---
+++ Sentura_Assignment/src/main/java/lk/ijse/Sentura_Assignment/SenturaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Sentura_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenturaAssignmentApplication.class, args);
+	}
+
+}
---
+++ Sentura_Assignment/src/test/java/lk/ijse/Sentura_Assignment/SenturaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Sentura_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenturaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
