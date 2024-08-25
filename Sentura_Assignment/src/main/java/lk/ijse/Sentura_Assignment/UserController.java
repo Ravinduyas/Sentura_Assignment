475a80d3381bcc43a3da721bcd4d5dee0f3d0063
ravinduyas <ravindu.yasanka21@gmail.com>
2024-08-25 09:58:54 +0530

    Post and get end points are created.

--- Sentura_Assignment/src/main/java/lk/ijse/Sentura_Assignment/UserController.java
+++ Sentura_Assignment/src/main/java/lk/ijse/Sentura_Assignment/UserController.java
@@ -3,6 +3,7 @@ package lk.ijse.Sentura_Assignment;
 import okhttp3.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.server.UnicastRef;
 
 import java.io.IOException;
 
@@ -38,5 +39,23 @@ public class UserController {
             }
         }
 
+    }
+    // user details (Get)
+    public String getUserDetails(String userId, UnicastRef client) throws IOException {
+        String url = "https://c19921ec78d74a588a8a54bd5ca6a550.weavy.io" + "/api/users/" + userId;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + "wys_rFR6AgDQfqrvOWjS04qNiJgkgDEbMZ4JhS1w")
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                throw new IOException("Failed to get user details: " + response.message());
+            }
+        }
     }
 }
--- Sentura_Assignment/src/main/java/lk/ijse/Sentura_Assignment/UserService.java
+++ Sentura_Assignment/src/main/java/lk/ijse/Sentura_Assignment/UserService.java
@@ -3,6 +3,12 @@ package lk.ijse.Sentura_Assignment;
 import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 @Service
 public class UserService {
@@ -15,5 +21,19 @@ public class UserService {
 
     public UserService() {
         this.client = new OkHttpClient();
+    }
+    @PostMapping
+    public String createUser(@RequestParam String uid, @RequestParam String name, @RequestParam(required = false) String email) throws IOException {
+        return userService.createUser(uid, name, email);
+    }
+
+    @GetMapping
+    public String listUsers(@RequestParam(defaultValue = "25") int take) throws IOException {
+        return userService.listUsers(take);
+    }
+
+    @GetMapping("/{userId}")
+    public String getUserDetails(@PathVariable String userId) throws IOException {
+        return userService.getUserDetails(userId);
     }
 }
